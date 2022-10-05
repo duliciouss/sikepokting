e1effb58e94cb5ce07f8d46dbd6039d16a704a8b
kandipwsl <kandipermana@gmail.com>
2022-10-05 23:05:32 +0700

    First commit

---
+++ resources/views/layouts/navigation-theme.blade.php
@@ -0,0 +1,24 @@
+<div class="main-menu-content">
+    <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+        <li class=" nav-item"><a class="d-flex align-items-center" href="index.html"><i data-feather="home"></i><span
+                    class="menu-title text-truncate" data-i18n="Home">Dashboard</span></a>
+        </li>
+        <li class=" nav-item"><a class="d-flex align-items-center" href="index.html"><i
+                    data-feather='dollar-sign'></i><span class="menu-title text-truncate"
+                    data-i18n="Home">Harga</span></a>
+        </li>
+        <li class=" navigation-header"><span data-i18n="Apps &amp; Pages">Data Master</span><i
+                data-feather="more-horizontal"></i>
+        </li>
+        <li class=" nav-item"><a class="d-flex align-items-center" href="index.html"><i data-feather="tag"></i><span
+                    class="menu-title text-truncate" data-i18n="Home">Komoditas</span></a>
+        </li>
+        <li class=" nav-item"><a class="d-flex align-items-center" href="index.html"><i
+                    data-feather='shopping-cart'></i><span class="menu-title text-truncate"
+                    data-i18n="Home">Pasar</span></a>
+        </li>
+        <li class=" nav-item"><a class="d-flex align-items-center" href="index.html"><i data-feather='users'></i><span
+                    class="menu-title text-truncate" data-i18n="Home">Pengguna</span></a>
+        </li>
+    </ul>
+</div>
