6a7ca5b86145ad65a50679a1edc06c18627183bd
kandipwsl <kandipermana@gmail.com>
2023-03-14 13:57:49 +0700

    Add header

--- app/Services/SilindaApiService.php
+++ app/Services/SilindaApiService.php
@@ -21,9 +21,11 @@ class SilindaApiService
     public function getToken()
     {
         Log::info($this->SILINDA_SCOPE);
-        $response = Http::timeout(3)->asForm()->post($this->SILINDA_TOKEN_URL . '/oauth2/token', [
+        $response = Http::timeout(3)->asForm()->post('/oauth2/token', [
             'grant_type' => 'client_credentials',
             'scope' => $this->SILINDA_SCOPE,
+        ])->withHeaders([
+            'Authorization' => $this->SILINDA_CLIENT_CREDENTIAL
         ]);
 
         Log::info($response);
