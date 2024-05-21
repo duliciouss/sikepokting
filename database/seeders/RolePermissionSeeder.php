470e5956a128f40cf77cd019ea78c4f0cc11fba0
Kandi Permana <kandipermana@gmail.com>
2024-05-21 15:47:05 +0700

    fix: use model permission

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -4,8 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Log;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
+use App\Models\Role;
+use App\Models\Permission;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -15,7 +15,7 @@ class RolePermissionSeeder extends Seeder
         $superadmin = Role::where('name', 'superadmin')->first();
         if ($superadmin) {
             $permissions = Permission::get();
-            Log::info($permissions);
+            Log::info($superadmin);
             $superadmin->syncPermissions($permissions);
         }
 
